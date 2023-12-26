4fb97bd93cb26ea18779a0401871c7ec534d7049
NauharDev <nauhar.kapur@mail.utoronto.ca>
2023-12-26 01:07:34 -0500

    added custom layouts to indiv. project page
    added custom layout functionality so that markdown from the yaml file is styled with custom components based on the figma theme. edited the yaml files to remove unnecessary text and distiguish between paragraphs (text), images and other components. Now, images are not wrapped with <p> elements, they are wrapped with <h6> elements and thus have their own styling.

--- pages/project/[id].tsx
+++ pages/project/[id].tsx
@@ -7,6 +7,7 @@ import { GetStaticPropsContext } from "next";
 import { ParsedUrlQuery } from "querystring";
 import TeamMember from "@/components/project/TeamMember";
 import ReactMarkdown from "react-markdown";
+import React from "react";
 type ContentData<T> = T & { content: string; slug: string };
 
 const IndividualProject = ({ found }: { found: ContentData<ProjectMetaData> }) => {
@@ -40,6 +41,50 @@ const IndividualProject = ({ found }: { found: ContentData<ProjectMetaData> }) =
     }
 
 
+    const H1Component = (props: { className?: string, children?: React.ReactNode }) => (
+        <div className={`px-[7.4vw] font-roboto-mono text-white text-[24px] font-[700] mb-[2vh]`}>
+            {props.children}
+            <div className={`bg-[#00349F] w-[8.1vw] h-[6px] ${props.className}`}></div>
+        </div>
+    );
+
+    const H2Component = (props: { className?: string, children?: React.ReactNode }) => (
+        <div className={`px-[9.5vw] font-roboto-mono text-white text-[20px] font-[700] mb-[2vh] mt-[3.5vh]`}>
+            {props.children}
+            <div className={`bg-[#00349F] w-[6.1vw] h-[4px] ${props.className}`}></div>
+        </div>
+    );
+
+    const UlComponent = (props: { className?: string, children?: React.ReactNode }) => (
+        <div className={`px-[9.5vw] font-roboto-mono text-white text-[16px] font-[400] mb-[3vh]`}>
+            {props.children}
+        </div>
+    );
+
+    const PComponent = (props: { className?: string, children?: React.ReactNode }) => {
+        return (
+            <div className={`px-[9.5vw] font-roboto-mono text-white text-[16px] font-[400] mb-[3vh]`}>
+                {props.children}
+            </div>
+        )
+    }
+
+    const ImgComponent = (props: {src?: string, alt?: string}) => {
+        return (
+            <div className="rounded-md">
+                <img src={props.src} alt={props.alt}/>
+            </div>
+        );
+    }
+
+    const H6Component = (props: {className?: string, children?: React.ReactNode}) => {
+        return (
+            <div className="px-[9.5vw] inline-block mb-[3vh]">
+                {props.children}
+            </div>
+        );
+    }
+
     return (
 
         <>
@@ -58,26 +103,33 @@ const IndividualProject = ({ found }: { found: ContentData<ProjectMetaData> }) =
                     </div>
 
                 </div>
-                <div className="px-[7.4vw] font-roboto-mono text-white text-[20px] font-[700] mb-[3vh]">
+                <div className="px-[7.4vw] font-roboto-mono text-white text-[24px] font-[700] mb-[3vh]">
                     <div className="mb-[1vh]">Description</div>
                     <div className="bg-[#00349F] w-[8.1vw] h-[6px]"></div>
                 </div>
                 <div className="px-[9.5vw] font-roboto-mono text-white font-[400] text-[14px] mb-[5vh]">
                     {found.description}
                 </div>
-                <div className="px-[7.4vw] font-roboto-mono text-white text-[20px] font-[700] mb-[3vh]">
-                    <div className="mb-[1vh]">What We Are Looking For</div>
-                    <div className="bg-[#00349F] w-[8.1vw] h-[6px]"></div>
-                </div>
-                <div className="px-[9.5vw] font-roboto-mono text-white font-[400] text-[14px] mb-[5vh]">
-                <ReactMarkdown>{found.content}</ReactMarkdown>
-                </div>
+                <ReactMarkdown
+                    components={{
+                        h1: ({ children, className = "mt-[1vh]", ...props }) => <H1Component className={className} {...props}>{children}</H1Component>,
+                        h2: ({ children, className = "mt-[1vh]", ...props }) => <H2Component className={className} {...props}>{children}</H2Component>,
+                        ul: ({ children, ...props }) => <UlComponent {...props}>{children}</UlComponent>,
+                        p: ({ children, ...props }) => <PComponent  {...props}>{children}</PComponent>,
+                        img: ({src, alt}) => <ImgComponent src={src} alt={alt}/>, 
+                        h6: ({children, className}) => <H6Component>{children}</H6Component>, 
+
+                    }}>
+                    {found.content}
+                </ReactMarkdown>
                 <div className="px-[7.4vw] font-roboto-mono text-white text-[20px] font-[700] mb-[6vh]">
                     <div className="mb-[1vh]">The Team</div>
                     <div className="bg-[#00349F] w-[8.1vw] h-[6px]"></div>
                 </div>
                 <div className="px-[9.5vw]">
                     {teamMemberData}
+
+
 
                 </div>
 
