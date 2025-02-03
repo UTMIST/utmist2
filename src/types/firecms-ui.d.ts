30205d3f1d2f1a3edecc17d139bf00349ba58375
Zain <zainhmoustafa@gmail.com>
2025-02-03 03:24:47 -0500

    thank the heavens, for I have prospered

---
+++ src/types/firecms-ui.d.ts
@@ -0,0 +1,5 @@
+   // src/@types/firecms-ui.d.ts
+   declare module '@firecms/ui/tailwind.config.js' {
+    const config: any; // or specify a more precise type if known
+    export default config;
+}
